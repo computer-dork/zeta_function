d03e3195b94b22c36de2d12f7c751a0dbf7974aa
Jarrod <jarrod@Jarrods-MBP.attlocal.net>
2022-05-11 13:06:25 -0500

    update file names to line up with descriptions in article.

--- rz_refresh_rate.py
+++ rz_refresh_rate.py
@@ -3,20 +3,34 @@ import numpy as np
 
 from manim import *
 
-class RiemanZetaZeroes(Scene):
+class RiemannZeta(Scene):
     def construct(self):
 
-        # set framerate for output
-        framerate = 60
-
-
-        # Build groups to hold traces
+        # build groups to hold traces
         trace_group_1 = Group()
         trace_group_2 = Group()
         trace_group_3 = Group()
         trace_group_4 = Group()
 
         # start iterating
+        for i in np.arange(1, 100, 2):
+
+            # build coords
+            z1 = mpmath.zeta(complex((1/2), i))
+            a1 = float(z1.real)
+            b1 = float(z1.imag)
+            c1 = i
+
+            # build coords 1 point in the future
+            zz1 = mpmath.zeta(complex((1/2), (i+2)))
+            d1 = float(zz1.real)
+            e1 = float(zz1.imag)
+            f1 = i + 2
+
+            #draw this portion of the line
+            rz_trace_1 = Line(start=[a1,b1,c1],end=[d1,e1,f1],color=BLUE_D,stroke_width=2)
+            trace_group_1.add(rz_trace_1)
+
         for i in np.arange(1, 100, 1):
 
             # build coords
@@ -26,25 +40,7 @@ class RiemanZetaZeroes(Scene):
             c1 = i
 
             # build coords 1 point in the future
-            zz1 = mpmath.zeta(complex((1/2), (i+rate)))
-            d1 = float(zz1.real)
-            e1 = float(zz1.imag)
-            f1 = i + 1
-
-            #draw this portion of the line
-            rz_trace_1 = Line(start=[a1,b1,c1],end=[d1,e1,f1],color=BLUE_D,stroke_width=2)
-            trace_group_1.add(rz_trace_1)
-
-        for i in np.arange(1, 100, 1):
-
-            # build coords
-            z1 = mpmath.zeta(complex((1/2), .75*i))
-            a1 = float(z1.real)
-            b1 = float(z1.imag)
-            c1 = i
-
-            # build coords 1 point in the future
-            zz1 = mpmath.zeta(complex((1/2), .75*(i+rate)))
+            zz1 = mpmath.zeta(complex((1/2), (i+1)))
             d1 = float(zz1.real)
             e1 = float(zz1.imag)
             f1 = i + 1
@@ -53,34 +49,34 @@ class RiemanZetaZeroes(Scene):
             rz_trace_2 = Line(start=[a1,b1,c1],end=[d1,e1,f1],color=BLUE_D,stroke_width=2)
             trace_group_2.add(rz_trace_2)
 
-        for i in np.arange(1, 100, 1):
+        for i in np.arange(1, 100, .5):
 
             # build coords
-            z1 = mpmath.zeta(complex((1/2), .5*i))
+            z1 = mpmath.zeta(complex((1/2), i))
             a1 = float(z1.real)
             b1 = float(z1.imag)
             c1 = i
 
             # build coords 1 point in the future
-            zz1 = mpmath.zeta(complex((1/2), .5*(i+rate)))
+            zz1 = mpmath.zeta(complex((1/2), (i+.5)))
             d1 = float(zz1.real)
             e1 = float(zz1.imag)
-            f1 = i + .1
+            f1 = i + .5
 
             #draw this portion of the line
             rz_trace_3 = Line(start=[a1,b1,c1],end=[d1,e1,f1],color=BLUE_D,stroke_width=2)
             trace_group_3.add(rz_trace_3)
 
-        for i in np.arange(1, 100, 1):
+        for i in np.arange(1, 100, .1):
 
             # build coords
-            z1 = mpmath.zeta(complex((1/2), .25*i))
+            z1 = mpmath.zeta(complex((1/2), i))
             a1 = float(z1.real)
             b1 = float(z1.imag)
             c1 = i
 
             # build coords 1 point in the future
-            zz1 = mpmath.zeta(complex((1/2), .25*(i+rate)))
+            zz1 = mpmath.zeta(complex((1/2), (i+.1)))
             d1 = float(zz1.real)
             e1 = float(zz1.imag)
             f1 = i + .1
@@ -102,11 +98,11 @@ class RiemanZetaZeroes(Scene):
         trace_group_3.shift(DOWN * 2, LEFT * 3).scale(.4)
 
         self.add(trace_group_4)
-        trace_group_4.shift(DOWN * 2, RIGHT * 4).scale(.4)
+        trace_group_4.shift(DOWN * 2, RIGHT * 3).scale(.4)
 
         # build latex for each resolution
         trace_1_text_string = '\\zeta(\\frac{1}{2} + it)'
-        trace_1_rt_string = 'S(t) = 1, L(t) = 100, R(t) = 1'
+        trace_1_rt_string = 'S(t) = 1, L(t) = 100, R(t) = 2'
         trace_1_text = MathTex(trace_1_text_string)
         trace_1_rt = MathTex(trace_1_rt_string)
         self.add(trace_1_text)
@@ -114,7 +110,7 @@ class RiemanZetaZeroes(Scene):
         self.add(trace_1_rt)
         trace_1_rt.next_to(trace_1_text, DOWN, buff=.1).scale(.5)
 
-        trace_2_text_string = '\\zeta(\\frac{1}{2} + \\frac{3}{4}it)'
+        trace_2_text_string = '\\zeta(\\frac{1}{2} + it)'
         trace_2_rt_string = 'S(t) = 1, L(t) = 100, R(t) = 1'
         trace_2_text = MathTex(trace_2_text_string)
         trace_2_rt = MathTex(trace_2_rt_string)
@@ -123,8 +119,8 @@ class RiemanZetaZeroes(Scene):
         self.add(trace_2_rt)
         trace_2_rt.next_to(trace_2_text, DOWN, buff=.1).scale(.5)
 
-        trace_3_text_string = '\\zeta(\\frac{1}{2} + \\frac{1}{2}it)'
-        trace_3_rt_string = 'S(t) = 1, L(t) = 100, R(t) = 1'
+        trace_3_text_string = '\\zeta(\\frac{1}{2} + it)'
+        trace_3_rt_string = 'S(t) = 1, L(t) = 100, R(t) = 0.5'
         trace_3_text = MathTex(trace_3_text_string)
         trace_3_rt = MathTex(trace_3_rt_string)
         self.add(trace_3_text)
@@ -132,8 +128,8 @@ class RiemanZetaZeroes(Scene):
         self.add(trace_3_rt)
         trace_3_rt.next_to(trace_3_text, DOWN, buff=.1).scale(.5)
 
-        trace_4_text_string = '\\zeta(\\frac{1}{2} + \\frac{1}{4}it)'
-        trace_4_rt_string = 'S(t) = 1, L(t) = 100, R(t) = 1'
+        trace_4_text_string = '\\zeta(\\frac{1}{2} + it)'
+        trace_4_rt_string = 'S(t) = 1, L(t) = 100, R(t) = 0.1'
         trace_4_text = MathTex(trace_4_text_string)
         trace_4_rt = MathTex(trace_4_rt_string)
         self.add(trace_4_text)
--- rz_refresh_rate_overlap.py
+++ rz_refresh_rate_overlap.py
@@ -3,39 +3,43 @@ import numpy as np
 
 from manim import *
 
-class RiemanZetaZeroes(Scene):
+class RiemannZeta(Scene):
     def construct(self):
 
         # build latex
-        time_text = 'S(t) = 1, L(t) = 100, R(t) = 1'
-        timing = MathTex(time_text)
-
         text_1 = '\\zeta(\\frac{1}{2} + it)'
+        time_text_1 = 'S(t) = 1, L(t) = 100, R(t) = 2'
         values_1 = MathTex(text_1,color=BLUE_D)
+        times_1 = MathTex(time_text_1,color=BLUE_D)
 
-        text_2 = '\\zeta(\\frac{1}{2} + \\frac{3}{4}it)'
+        text_2 = '\\zeta(\\frac{1}{2} + it)'
+        time_text_2 = 'S(t) = 1, L(t) = 100, R(t) = 1'
         values_2 = MathTex(text_2,color=PURPLE_A)
+        times_2 = MathTex(time_text_2,color=PURPLE_A)
 
-        text_3 = '\\zeta(\\frac{1}{2} + \\frac{1}{2}it)'
+        text_3 = '\\zeta(\\frac{1}{2} + it)'
+        time_text_3 = 'S(t) = 1, L(t) = 100, R(t) = 0.5'
         values_3 = MathTex(text_3,color=YELLOW_D)
+        times_3 = MathTex(time_text_3, color=YELLOW_D)
 
-        text_4 = '\\zeta(\\frac{1}{2} + \\frac{1}{4}it)'
+        text_4 = '\\zeta(\\frac{1}{2} + it)'
+        time_text_4 = 'S(t) = 1, L(t) = 100, R(t) = 0.1'
         values_4 = MathTex(text_4,color=ORANGE)
+        times_4 = MathTex(time_text_4, color = ORANGE)
 
         values_group = VGroup(
-            timing,
             values_1,
+            times_1,
             values_2,
+            times_2,
             values_3,
-            values_4
+            times_3,
+            values_4,
+            times_4
         ).arrange(DOWN, aligned_edge=LEFT)
 
         self.add(values_group)
-        values_group.to_corner(UL).shift(RIGHT*.35,UP*.8).scale(.5)
-
-
-        # set framerate for output
-        framerate = 60
+        values_group.to_corner(UL).shift(RIGHT*.35,UP*2.4).scale(.5)
 
         # add a complex plane
         plane = ComplexPlane(
@@ -52,76 +56,76 @@ class RiemanZetaZeroes(Scene):
         rz_group = Group()
 
         # start iterating each step
-        for i in np.arange(start, top, rate):
+        for i in np.arange(1, 100, 2):
 
             # build coords
-            z = mpmath.zeta(complex(.5, 1*i))
+            z = mpmath.zeta(complex(.5, i))
             a = float(z.real)
             b = float(z.imag)
             c = i
 
             # build coords 1 point in the future
-            zz = mpmath.zeta(complex(.5, 1*(i+rate)))
+            zz = mpmath.zeta(complex(.5, (i+2)))
             d = float(zz.real)
             e = float(zz.imag)
-            f = i + rate
+            f = i + 2
 
             # draw this portion of the line
             rz_trace_1 = Line(start=[a,b,c],end=[d,e,f],color=BLUE_D,stroke_width=2)
 
             rz_group.add(rz_trace_1)
 
-        for i in np.arange(start, top, rate):
+        for i in np.arange(1, 100, 1):
 
             # build coords
-            z = mpmath.zeta(complex(.5, .75*i))
+            z = mpmath.zeta(complex(.5, i))
             a = float(z.real)
             b = float(z.imag)
             c = i
 
             # build coords 1 point in the future
-            zz = mpmath.zeta(complex(.5, .75*(i+rate)))
+            zz = mpmath.zeta(complex(.5, (i+1)))
             d = float(zz.real)
             e = float(zz.imag)
-            f = i + rate
+            f = i + 1
 
             # draw this portion of the line
             rz_trace_2 = Line(start=[a,b,c],end=[d,e,f],color=PURPLE_D,stroke_width=2)
 
             rz_group.add(rz_trace_2)
 
-        for i in np.arange(start, top, rate):
+        for i in np.arange(1, 100, .5):
 
             # build coords
-            z = mpmath.zeta(complex(.5, .5*i))
+            z = mpmath.zeta(complex(.5, i))
             a = float(z.real)
             b = float(z.imag)
             c = i
 
             # build coords 1 point in the future
-            zz = mpmath.zeta(complex(.5, .5*(i+rate)))
+            zz = mpmath.zeta(complex(.5, (i+.5)))
             d = float(zz.real)
             e = float(zz.imag)
-            f = i + rate
+            f = i + .5
 
             # draw this portion of the line
             rz_trace_3 = Line(start=[a,b,c],end=[d,e,f],color=YELLOW_D,stroke_width=2)
 
             rz_group.add(rz_trace_3)
 
-        for i in np.arange(start, top, rate):
+        for i in np.arange(1, 100, .1):
 
             # build coords
-            z = mpmath.zeta(complex(.5, .25*i))
+            z = mpmath.zeta(complex(.5, i))
             a = float(z.real)
             b = float(z.imag)
             c = i
 
             # build coords 1 point in the future
-            zz = mpmath.zeta(complex(.5, .25*(i+rate)))
+            zz = mpmath.zeta(complex(.5, (i+.1)))
             d = float(zz.real)
             e = float(zz.imag)
-            f = i + rate
+            f = i + .1
 
             # draw this portion of the line
             rz_trace_4 = Line(start=[a,b,c],end=[d,e,f],color=ORANGE,stroke_width=2)
